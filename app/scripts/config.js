f8104ab8e01aa8a8a08384abe5e3b9c2929f0864
Sharp Hall <shall@azavea.com>
2015-06-30 15:45:44 -0400

    Add museum names to typeahead

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -18,7 +18,11 @@
         },
         cartodb: {
             visId: 'a4ed4438-ffec-11e4-8b9a-0e9d821ea90d',
-            account: 'azavea-demo'
+            account: 'azavea-demo',
+            tableName: 'mudf15q1int'
+        },
+        typeahead: {
+            results: 10
         },
         // Configures various items in the app based on whether we are debugging or not
         debug: true,
