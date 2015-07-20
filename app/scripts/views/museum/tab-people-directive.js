64e9f30f6ef9ba3d7cdda21f3642d3c86077a349
CloudNiner <andrew.fink.11@gmail.com>
2015-07-20 12:13:10 -0500

    Move people chart tab to directive
    Provide separation of concerns so we don't clutter the
    museum controller with a bunch of chart stuff.

---
+++ app/scripts/views/museum/tab-people-directive.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    /* ngInject */
+    function PeopleTabController($log, ACSAggregate, ACSVariables) {
+        var ctl = this;
+
+        initialize();
+
+        function initialize() {
+            ctl.acsVariables = acsVariables;
+            $log.info(ctl.data);
+        }
+    }
+
+    /* ngInject */
+    function imlsTabPeople() {
+        var module = {
+            restrict: 'E',
+            templateUrl: 'scripts/views/museum/tab-people-partial.html',
+            scope: {
+                data: '='
+            },
+            controller: 'PeopleTabController',
+            controllerAs: 'people',
+            bindToController: true
+        };
+        return module;
+    }
+
+    angular.module('imls.views.museum')
+    .controller('PeopleTabController', PeopleTabController)
+    .directive('imlsTabPeople', imlsTabPeople);
+
+})();
