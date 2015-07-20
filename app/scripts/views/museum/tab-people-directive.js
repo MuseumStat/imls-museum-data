ec5fb472ac74e5d7ae29cdd9b40ddf9c9a3fdbed
CloudNiner <andrew.fink.11@gmail.com>
2015-07-20 15:17:04 -0500

    Add helper function to create bar graphs
    Should probably be done in directive, but this is quicker.
    Did not use nvd3-angular plugin, it seems to be a dead project
        No new versions since last year... :(

--- app/scripts/views/museum/tab-people-directive.js
+++ app/scripts/views/museum/tab-people-directive.js
@@ -5,11 +5,37 @@
     function PeopleTabController($log, $scope, ACSAggregate, ACSVariables) {
         var ctl = this;
 
+        var raceVariables = [
+            // Race -- bar
+            'B02001_002E',
+            'B02001_003E',
+            'B02001_004E',
+            'B02001_005E',
+            'B02001_006E',
+            'B02001_007E',
+            'B02001_008E'
+        ];
+        var employmentVariables = [
+            'B23025_004E',
+            'B23025_005E',
+            'B23025_006E',
+            'B23025_007E'
+        ];
+        var houseLangVariables = [
+            'B16002_002E',
+            'B16002_003E',
+            'B16002_006E',
+            'B16002_009E',
+            'B16002_012E'
+        ];
+
+        var raceBarChart = null;
+
         initialize();
 
         function initialize() {
             ctl.acsVariables = ACSVariables;
-            onSumDataChanged(ctl.data);
+            ctl.charts = {};
 
             $scope.$watch(function () { return ctl.data; }, onSumDataChanged);
         }
@@ -19,7 +45,42 @@
             if (newData && newData.headers && newData.data) {
                 ctl.data = newData;
                 ctl.sumData = ACSAggregate.sum(ctl.data);
+
+                drawBarChart('race', generateSeries(ctl.sumData, raceVariables));
+                drawBarChart('employment', generateSeries(ctl.sumData, employmentVariables));
+                drawBarChart('household-language', generateSeries(ctl.sumData, houseLangVariables));
             }
+        }
+
+        function drawBarChart(key, series) {
+            var datum = [{
+                key: key,
+                values: series
+            }];
+            if (!ctl.charts[key]) {
+                var chart = nv.models.discreteBarChart()
+                    .x(function(d) { return d.label; })
+                    .y(function(d) { return d.value; })
+                    .staggerLabels(true)
+                    .color(function () { return '#7779b1'; })
+                    //.staggerLabels(historicalBarChart[0].values.length > 8)
+                    //.showValues(true)
+                    .margin({right: 10});
+                nv.utils.windowResize(chart.update);
+                ctl.charts[key] = chart;
+            }
+            d3.select('#bar-chart-' + key + ' svg')
+                .datum(datum)
+                .call(ctl.charts[key]);
+        }
+
+        function generateSeries(data, variables) {
+            return _.map(variables, function (variable) {
+                return {
+                    value: data[variable],
+                    label: ACSVariables[variable]
+                };
+            });
         }
     }
 
