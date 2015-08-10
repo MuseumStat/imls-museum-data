f2e5481f1f980f0694caa055ff49c48f7b34a169
Sharp Hall <shall@azavea.com>
2015-08-10 13:02:35 -0400

    Add pie charts

--- app/scripts/acs/acs-graphs.js
+++ app/scripts/acs/acs-graphs.js
@@ -9,9 +9,24 @@
 
         var module = {
             drawBarChart: drawBarChart,
-            generateSeries: generateSeries
+            drawPieChart: drawPieChart,
+            generateSeries: generateSeries,
+            updateCharts: updateCharts
         };
         return module;
+
+        function addElipses(text, max) {
+            if (text.length > max) {
+                return text.substr(0, max-1) + '...';
+            }
+            return text;
+        }
+
+        function updateCharts() {
+            _.forIn(charts, function (chart) {
+                chart.update();
+            });
+        }
 
         function drawBarChart(key, series, forceRedraw) {
             var datum = [{
@@ -20,7 +35,7 @@
             }];
             if (forceRedraw || !charts[key]) {
                 var chart = nv.models.discreteBarChart()
-                    .x(function(d) { return d.label.substr(0,8) + '...'; })
+                    .x(function(d) { return addElipses(d.label, 9); })
                     .y(function(d) { return d.value; })
                     .tooltipContent(function(d) {
                         return '<b>' + d.data.label + ': </b>' + d.data.value;
@@ -33,6 +48,25 @@
             }
             d3.select('#bar-chart-' + key + ' svg')
                 .datum(datum)
+                .call(charts[key]);
+        }
+
+        function drawPieChart(key, data, forceRedraw) {
+            if (forceRedraw || !charts[key]) {
+                var chart = nv.models.pieChart()
+                    .x(function(d) { return addElipses(d.label, 8); })
+                    .y(function(d) { return d.value; })
+                    .tooltipContent(function(d) {
+                        return '<b>' + d.data.label + ': </b>' + d.data.value;
+                    })
+                    .color(function () { return '#7779b1'; })
+                    .margin({right: 10})
+                    .showLegend(false);
+                nv.utils.windowResize(chart.update);
+                charts[key] = chart;
+            }
+            d3.select('#pie-chart-' + key + ' svg')
+                .datum(data)
                 .call(charts[key]);
         }
 
