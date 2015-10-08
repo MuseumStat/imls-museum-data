56cd8d30fe2aeee31032974e8e555dcb201eef41
CloudNiner <andrew.fink.11@gmail.com>
2015-10-08 15:34:21 -0400

    Ensure demographics layer respects vis default options
    Fixes CartoDB logo showing by default when the
      demographics layer is added.

--- app/scripts/map/cartodb-vis-directive.js
+++ app/scripts/map/cartodb-vis-directive.js
@@ -54,9 +54,9 @@
             map = vis.getNativeMap();
 
             if (ctl.demographics) {
-                cartodb.createLayer(map, Config.cartodb.demographicVisUrl, {
-                    tooltip: true
-                }).addTo(map).done(function (layer) {
+                var demographicsOptions = angular.extend({}, defaultOptions, {tooltip: true});
+                cartodb.createLayer(map, Config.cartodb.demographicVisUrl, demographicsOptions)
+                .addTo(map).done(function (layer) {
                     $('div.cartodb-legend-stack').filter(':first').css('bottom', '150px');
                     ctl.sublayers = layer.getSubLayers();
                     ctl.onSublayerChange(ctl.sublayers[-1]);
